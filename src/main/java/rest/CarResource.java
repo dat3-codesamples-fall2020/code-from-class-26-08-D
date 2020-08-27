a20d95b9e242eb2a46cce451809e1cc73cff8f8d
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2020-08-27 14:29:50 +0200

    pathparam

--- src/main/java/rest/CarResource.java
+++ src/main/java/rest/CarResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("car")
@@ -19,24 +20,25 @@ public class CarResource {
 
     @Context
     private UriInfo context;
-    
+
     private static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static List<CarDTO> cars = new ArrayList();
-   
+
     /**
      * Creates a new instance of CarResource
      */
     public CarResource() {
-        if(cars.isEmpty()){
-            cars.add(new CarDTO("Volvo",12000,1995));
-            cars.add(new CarDTO("ww",11000,1995));
-            cars.add(new CarDTO("Toyata",14000,1995));
-            cars.add(new CarDTO("Saab",14000,1995));
+        if (cars.isEmpty()) {
+            cars.add(new CarDTO("Volvo", 12000, 1995));
+            cars.add(new CarDTO("ww", 11000, 1995));
+            cars.add(new CarDTO("Toyata", 14000, 1995));
+            cars.add(new CarDTO("Saab", 14000, 1995));
         }
     }
 
     /**
      * Retrieves representation of an instance of rest.CarResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
@@ -45,34 +47,51 @@ public class CarResource {
         //TODO return proper representation object
         return "[]";
     }
+
     @Path("driver")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson2() {
         //TODO return proper representation object
-       
+
         return "{\"name\":\"Kurt Wonnegut\"}";
     }
-    
+
     @Path("carobject")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson3() {
-        CarDTO car = new CarDTO("Volvo",12000,1995);
+        CarDTO car = new CarDTO("Volvo", 12000, 1995);
         String jsonString = GSON.toJson(car);
-        return jsonString; 
+        return jsonString;
     }
-    
+
+    @Path("carbyid/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCarById(@PathParam("id") int id) {
+        try {
+        CarDTO car = cars.get(id);
+        String jsonString = GSON.toJson(car);
+        return jsonString;
+        }catch(Exception e){
+            String err = "{\"err\":404}";
+            return err;
+        }
+    }
+   
+
     @Path("allcars")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson4() {
         String jsonString = GSON.toJson(cars);
-        return jsonString; 
+        return jsonString;
     }
 
     /**
      * PUT method for updating or creating an instance of CarResource
+     *
      * @param content representation for the resource
      */
     @PUT
